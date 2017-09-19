5a9277b38a443b81f57200ff217cf436e7475d26
suman.paria@gmail.com <suman.paria@gmail.com>
2017-09-19 10:09:56 -0700

    Electron with Handsontable

---
+++ README.md
@@ -0,0 +1,4 @@
+# Electron using Handsontable
+    using node 6.11.2 and npm 3.10.10
+    1. npm install
+    2. npm run start
---
+++ src/index.js
@@ -0,0 +1,60 @@
+'use strict';
+const electron = require('electron');
+const dialog = electron.dialog;
+const Menu = electron.Menu;
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+var path = require('path');
+
+
+var mainWindow = null;
+var _exec;
+
+var shouldQuit = app.makeSingleInstance(function (commandLine, workingDirectory) {
+    if (mainWindow) {
+        if (mainWindow.isMinimized()) mainWindow.restore();
+        mainWindow.focus();
+    }
+});
+
+if (shouldQuit) {
+    app.quit();
+    return;
+}
+
+app.on('ready', function () {
+
+    mainWindow = new BrowserWindow({
+        title: app.getName(),
+        width: 1024,
+        height: 768,
+        backgroundColor: '#080d19',
+        webPreferences: {
+            nodeIntegration: false
+        },
+        toolbar: false,
+        show: false,
+        icon: path.join(__dirname, 'favicon.ico')
+    });
+
+    mainWindow.loadURL("file://" + __dirname + "/index.html");
+    mainWindow.setMenu(null);
+    mainWindow.setMenuBarVisibility(true);
+    mainWindow.setAutoHideMenuBar(true);
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show();
+        mainWindow.maximize();
+    });
+
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+        app.exit(0);
+    });
+});
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
+});
